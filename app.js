11e1e6d68e2c7494f99d371357882a4456fc250d
root <root@ip-172-31-44-15.eu-central-1.compute.internal>
2017-11-12 17:44:00 +0000

    Added https

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 var express = require('express');
+var helmet = require('helmet');
+var https = require('https');
+var fs = require('fs');
 var app = express();
+
+app.use(helmet());
 
 app.get('/', function(req, res) {
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ a: 1 }));
+  res.send('up');
 });
+
+/*
+//	To certificate
+app.get('/.well-known/acme-challenge/QVLCinHX6u3fEIx_VOuhUSRh_0KLJ5HtG23AdpzqNrE', function(req, res) {
+  res.sendFile('./data', { root: __dirname });
+});
+*/
 
 app.locals.connections = [];
+
 require('./app/routes')(app);
 
-app.listen(3000, function() {
-  console.log('Listening on port 3000...')
+https.createServer({
+  key: fs.readFileSync('/etc/letsencrypt/live/noty-system.tk/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/noty-system.tk/cert.pem'),
+  ca: fs.readFileSync('/etc/letsencrypt/live/noty-system.tk/chain.pem')
+}, app).listen(443);
+
+/*
+app.listen(80, function() {
+  console.log('Listening on port 3000..')
 });
+*/
